39d6f528627015fe6dcf3dd1f7fa4105dc87700c
bekinblack <aapanasov@gmail.com>
2023-04-30 16:12:28 +0300

    refactor: change key button ids

--- src/components/keyboard/utils/getKeyIds.js
+++ src/components/keyboard/utils/getKeyIds.js
@@ -1,7 +1,8 @@
 import layouts from '../layouts';
+import keymap from './keymap';
 
 function getKeyIds() {
-  return layouts.english.normal.map((key) => `id-${key}`);
+  return layouts.english.normal.map((_, index) => keymap[index]);
 }
 
 export default getKeyIds();
